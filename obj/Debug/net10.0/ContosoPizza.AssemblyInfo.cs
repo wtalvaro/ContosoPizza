21c7b0f79e7d6f041015fd3e63afc6e58008c8fd
wtalvaro <wagneralvaro80@gmail.com>
2026-01-03 19:23:08 +0000

    refactor: move WeatherForecast para a pasta Models
    Organiza a estrutura do projeto movendo o modelo de clima para o diretório de Models.
    
    Atualiza o namespace conforme a nova localização.

--- obj/Debug/net10.0/ContosoPizza.AssemblyInfo.cs
+++ obj/Debug/net10.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e323465a57c1f5ebb30c5069fb0cb215428840d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a183bdcc49b65b33450cbf6cf28f981f8746a48")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
